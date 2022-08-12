c3b7295ecc1d760dfab571bd74b664610548e85a
Nuttakorn <nuttakorn.tub@gmail.com>
2022-08-12 23:58:09 +0700

    learning

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	handler "fiber/handler/helloworld"
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New(fiber.Config{
-		Prefork:       true,
+		Prefork:       false,
 		StrictRouting: true,
 		CaseSensitive: true,
 		ColorScheme:   fiber.DefaultColors,
@@ -21,8 +22,20 @@ func main() {
 		fmt.Println("I'm a child process")
 	}
 
-	app.Get("/", handler.HelloWorld)
+	app.Get("/hello", handler.HelloWorld)
 	app.Get("/test", handler.Test)
+
+	micro := fiber.New()
+	micro.Get("/hello2", handler.HelloWorld)
+	micro.Static("/", "./public", fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		Browse:        true,
+		Index:         "hello.html",
+		CacheDuration: 10 * time.Second,
+		MaxAge:        3600,
+	})
+	app.Mount("/static", micro)
 
 	app.Listen("127.0.0.1:3000")
 
